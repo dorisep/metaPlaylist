dfcb85dd34e824040715d2b60341629494d44486
Ed Doris <ed.doris@gmail.com>
2020-12-27 16:21:43 -0600

    create playlist added
    Figured out playlist creation working on collecting track ids from album ids

--- jupyter_notebooks/.ipynb_checkpoints/config.py
+++
@@ -1,2 +0,0 @@
-client_id = "6f2a7816998d4d7e992f1dba605ccd33"
-client_secret = "f1029bcc83634b4c9497ec0bcd701498"
---
+++ jupyter_notebooks/config.py
@@ -0,0 +1,4 @@
+spotify_user_id = "ed.doris"
+client_id = "6f2a7816998d4d7e992f1dba605ccd33"
+client_secret = "f1029bcc83634b4c9497ec0bcd701498"
+redirect_uri = "http://localhost:8888/callback/"
