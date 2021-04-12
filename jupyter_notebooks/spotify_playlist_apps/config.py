8894ea83b3b6ef2746ec15a9d9e9818f9c6c426e
dorisep <ed.doris@gmail.com>
2021-04-12 17:29:17 -0500

    Updated readme-feature added
    Refresh playlist token added to spotify_playlist_test
    removed config file

--- jupyter_notebooks/spotify_playlist_apps/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-spotify_pass = 'cr^ne2rucKSY'
-spotify_user_id = 'ed.doris'
-client_id = '6f2a7816998d4d7e992f1dba605ccd33'
-client_secret = 'f1029bcc83634b4c9497ec0bcd701498'
-redirect_uri = 'http://localhost:8888/callback/'
-playlist_token = 'BQCssylM7YmQAyYk51bjvd5tSqhDLgDsRjkbF2Y6p4MLIMw1X1M7CX7po05mbpuuwhL1PisuM5hrAiD68JtVWRlSJUxZsP4qUrxQuHGDrSebPA9nyrgXVf6P7vTCb90Q-V3TCxdU1YcegqvG5UrIE7xpxtAou_IqZcSQ0slnJvaKXpow5BN_jIW9F_MKCw'
-path = os.path.join('..','..', 'data', 'test','test2.csv')
