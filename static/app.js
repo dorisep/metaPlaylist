dc670b24f4d464f4af70a83cdf06ffcd8cc27099
dorisep <ed.doris@gmail.com>
2021-05-02 22:07:42 -0500

    Down to apps

--- static/app.js
+++
@@ -1,7 +0,0 @@
-d3.csv("../data/albums_2020a.csv").then(albumsData =>
-    {
-        // console.log(albumsData)
-        albumsData.forEach(row => {
-            console.log(row.name);
-        })
-    });
