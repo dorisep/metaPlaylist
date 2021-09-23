210fe9f88de04d5bdeaf78884516abde73096453
dorisep <ed.doris@gmail.com>
2021-09-23 10:30:50 -0500

    added feature to GUI
    introduced a bar chart to the tkinter gui

--- apps/tk_test.py
+++ apps/tk_test.py
@@ -1,36 +1,44 @@
 from tkinter import *
-from PIL import ImageTk, Image
+import numpy as np
+import matplotlib
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+import tkinter as tk
 from meta_scrape import metaScorePages
 from playlist_app import get_week_num
-import numpy as np
-import matplotlib.pyplot as plt
-# set instance of tkinter
-root = Tk()
-# set title
-root.title('Create Playlist from meta_scrape')
-# set window size
-root.geometry('800x800')
-# create variable for weeknum and default input for 
-week_num = IntVar(root, value = get_week_num())
-# label widget
-def myClick():
+
+matplotlib.use('TkAgg')
+#set instance of tk
+window = tk.Tk()
+#set title
+window.title('Create Playlist from meta_scrape')
+#set window size
+window.geometry('800x800')
+# window.state('zoomed')
+# set week_num variable to current week
+week_num = IntVar(window, value = get_week_num())
+# import scrape module and set to week_num variable to value in week field
+def scrape():
     metaScorePages(week_num.get())
-    print(week_num)
+# set plotting function
+def plot():
+    datalst = [31, 41, 59, 26, 53, 58, 97, 96, 36]
+    ff = Figure(figsize=(6,6), dpi=100)
+    xx = ff.add_subplot(111)
+    ind = np.arange(len(datalst))
+    rects1 = xx.bar(ind, datalst, 0.8)
+    canvas = FigureCanvasTkAgg(ff, master=window)
+    canvas.draw()
+    canvas.get_tk_widget().pack(side=tk.RIGHT)
+# create button for scrape
+scraper = tk.Button(window,text='Run Scrape for week number:',command=scrape, height=1,width=25,state='normal')
+scraper.place(x=12, y=20)
+# create field for week num variable
+week_field = Entry(window, textvariable=week_num, width=2)
+week_field.place(x=200, y=22)
+# create button for plot
+plotter = tk.Button(window,text='plot',command=plot, height=1,width=25,state='normal')
+plotter.place(x=12, y=45)
 
-def myClick2():
-    # create_playlist()
-    print()
-e = Entry(root, textvariable=week_num, width=2)
-
-# button
-myButton = Button(root, text='Run app for week: ', command=myClick)
-myButton2 = Button(root,text='Run create_playlist', command=myClick2)
-#  grid
-# myLabel1.grid(row=0, column=0)
-# myLabel2.grid(row=1, column=1)
-e.grid(row=2, column=1)
-myButton.grid(row=2, column=0)
-myButton2.grid(row=3, column=0)
-# event loop
-root.mainloop()
+window.mainloop()
 
