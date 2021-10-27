c540eed5171ca3cc30d8f76fe603c99391f44227
dorisep <ed.doris@gmail.com>
2021-10-26 20:29:40 -0500

    adding reviews to scrap

--- apps/meta_scrape.py
+++ apps/meta_scrape.py
@@ -24,43 +24,42 @@ def metaScrape(week_num):
     userN = []
     # create/initialize dictionary 
     albums_dict = {'artist':[], 'album':[], 'date':[], 'week_num':[], 'meta_score': [], 'user_score':[]}
-    # csv variables
-    output_path = os.path.join('..', 'data', 'meta_scrape.csv')
+
     soup_score.find_all('td', class_='clamp-summary-wrap')
     # create soup 
-    for artist in soup_score.find_all('td', class_='clamp-summary-wrap'):
+    for _ in soup_score.find_all('td', class_='clamp-summary-wrap'):
         # scrape album name
-        albums_dict['album'].append(artist.find('a', class_= 'title').text)
+        albums_dict['album'].append(_.find('a', class_= 'title').text)
         # scrape artist name and strip white space and extra characters
-        albums_dict['artist'].append(artist.find('div', class_='artist').text.strip().lstrip('by '))
+        albums_dict['artist'].append(_.find('div', class_='artist').text.strip().lstrip('by '))
         # scrape date
-        albums_dict['date'].append(artist.find('div', class_='clamp-details').find('span').text)
+        albums_dict['date'].append(_.find('div', class_='clamp-details').find('span').text)
         # scrape meta_score, handle for changes in class name, convert data type of score to int and append to dict
         # except set to pass since all alubms have a score
 
         try:
-            albums_dict['meta_score'].append(int(artist.find('div', class_='metascore_w large release positive').text))  
+            albums_dict['meta_score'].append(int(_.find('div', class_='metascore_w large release positive').text))  
         except:
             pass
         try:
-            albums_dict['meta_score'].append(int(artist.find('div', class_='metascore_w large release mixed').text))  
+            albums_dict['meta_score'].append(int(_.find('div', class_='metascore_w large release mixed').text))  
         except:
             pass 
         try:
-            albums_dict['meta_score'].append(int(artist.find('div', class_='metascore_w large release negative').text))  
+            albums_dict['meta_score'].append(int(_.find('div', class_='metascore_w large release negative').text))  
         except:
             pass
         # scrape user score, handle errors for tbd/class name and append to temp list
         try:
-            userP.append(float(artist.find('div', class_='metascore_w user large release positive').text))  
+            userP.append(float(_.find('div', class_='metascore_w user large release positive').text))  
         except:
             userP.append(0)
         try:
-            userM.append(float(artist.find('div', class_='metascore_w user large release mixed').text))  
+            userM.append(float(_.find('div', class_='metascore_w user large release mixed').text))  
         except:
             userM.append(0)
         try:
-            userN.append(float(artist.find('div', class_='metascore_w user large release negative').text))  
+            userN.append(float(_.find('div', class_='metascore_w user large release negative').text))  
         except:
             userN.append(0)
             
@@ -82,39 +81,38 @@ def metaScrape(week_num):
     fields = ['artist', 'album', 'date', 'week_num', 'meta_score', 'user_score'] 
     # create variable for data to be written
     data = zip(albums_dict['artist'], albums_dict['album'], albums_dict['date'], albums_dict['week_num'], albums_dict['meta_score'], albums_dict['user_score'])
-    # create file and write csv
-    # if page_num == 0 or False:
-    #     with open(output_path, 'w') as csvfile:
-    #         writer = csv.writer(csvfile)
-    #         writer.writerow(fields)
-    #         for d in data:
-    #             writer.writerow(d)
-    # else:
+    return albums_dict
+def scrape_reviews(albums_dict):
+    artists = albums_dict['artist']
+    for artist in artists:
+        url_for_reviews = f'https://www.metacritic.com/music/the-myth-of-the-happily-ever-after/{artist}'
+        user_agent = {'User-agent': 'Mozilla/5.0'}
+        # send response
+        response_reviews = requests.get(url_for_reviews, headers = user_agent)
+        # scrape website into variable to parse
+        soup_reviews = BeautifulSoup(response_reviews.text, 'html.parser')
+        print(soup_reviews)
+    playlist_app.create_playlist(week_num)
+    return albums_dict
+
+
+def write_csv(albums_dict):
+    # write dictionary to csv
+    # csv variables
+    output_path = os.path.join('..', 'data', 'meta_scrape.csv')
+    # create header
+    fields = ['artist', 'album', 'date', 'week_num', 'meta_score', 'user_score'] 
+    # create variable for data to be written
+    data = zip(
+            albums_dict['artist'], 
+            albums_dict['album'], 
+            albums_dict['date'], 
+            albums_dict['week_num'], 
+            albums_dict['meta_score'], 
+            albums_dict['user_score']
+            )
     with open(output_path, 'a') as csvfile:
         writer = csv.writer(csvfile)
-        for d in data:
+        for key, value in albums_dict.items():
             writer.writerow(d)
-    print(week_num)
     return playlist_app.create_playlist(week_num)
-
-# def metaScorePages(week_num):  
-#     url_pages = f'https://www.metacritic.com/browse/albums/release-date/new-releases/date'
-#     # set user agent for header
-#     user_agent = {'User-agent': 'Mozilla/5.0'}
-#     # send response
-#     response_pages = requests.get(url_pages, headers = user_agent)
-#     # scrape website into variable to parse
-#     soup_pages = BeautifulSoup(response_pages.text, 'html.parser')
-#     # set condition to check for one or multiple pages and pass the url to the scrape funtion accordingly
-#     if soup_pages.find('li', class_='page last_page') is None:
-#             url_for_scrape = 'https://www.metacritic.com/browse/albums/score/metascore/year/filtered'
-#             page_num = False
-#             metaScrape(week_num, url_for_scrape, page_num)
-#     else:
-#         pages = int(soup_pages.find('li', class_='page last_page').text)
-        
-#         for page_num in range(pages):
-#             url_for_scrape = f'{url_pages}&page={page_num}'
-#             metaScrape(url_pages, page_num)
-
-#     return playlist_app.create_playlist(week_num)
