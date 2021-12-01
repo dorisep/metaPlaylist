e7fbc0290abd164e668c11d45c3874a2a4d52d42
dorisep <ed.doris@gmail.com>
2021-11-30 23:25:52 -0600

    added feature
    created new file that collects basic queries into a dictionary

--- apps/db_read_test.py
+++ apps/db_read_test.py
@@ -1,21 +1,18 @@
 import sqlite3
 import json
+from basic_query_funcs import queries_dict
+# func for artist queries
 
-# def query_artist(artist):
-#     command = ("select * from albums where artist = ?;", [artist])
-#     return command
 
 # print(query_artist('Twin Shadow'))
 artist = 'Twin Shadow'
-# queries_dict = {"ar_query": query_artist}
-# test = queries_dict["ar_query"]("Twin Shadow")
-# create context manager
-command = (f"select * from albums where artist = '{artist}'")
 
+artist_query = queries_dict["ar_query"]("Twin Shadow")
+# create context manager
 with sqlite3.connect('meta_music.db') as conn:
 # create cursor
     cur = conn.cursor()
 # query db
-    cur.execute(command)
+    cur.execute(artist_query)
     query = cur.fetchall()
     print(json.dumps(query))
